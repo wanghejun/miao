687314f5116b48a685132b410654a52b774e80c3
HJ <865876163@qq.com>
2020-04-28 15:19:05 +0800

    lodashtest

--- lodash/wanghejun-lodash.js
+++ lodash/wanghejun-lodash.js
@@ -1,5 +1,13 @@
 var wanghejun = {
   chunk:function (arr,size=1) { 
-    
-   }
+
+   },
+   isNull:function (val) { 
+     if(val == null){
+      return true
+     }else{
+       return false
+
+     }
+    }
 }
