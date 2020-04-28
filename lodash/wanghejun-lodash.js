eb27930ec5dad7ac85e2fcadb20c982fd06d2a30
HJ <865876163@qq.com>
2020-04-28 16:46:41 +0800

    lodashtest

--- lodash/wanghejun-lodash.js
+++ lodash/wanghejun-lodash.js
@@ -1,4 +1,10 @@
 var wanghejun = {
+  /**
+   * 将数组分成每组size个
+   * @param {Array} arr 被拆分的数组
+   * @param {Number} size 每组大小
+   * @returns {Array[[]]}
+   */
   chunk:function (arr,size=1) { 
     var result = []
     var arrays = []
@@ -17,6 +23,11 @@ var wanghejun = {
     }
     return result
    },
+   /**
+    * 判断一个值是否null
+    * @param {*} val 判断的值
+    * @returns {boolean}
+    */
    isNull:function (val) { 
      if(val === null){
       return true
@@ -24,5 +35,15 @@ var wanghejun = {
        return false
 
      }
+    },/**
+     * 
+     * @param {Array} array 
+     * @param {Array} values 
+     */
+    concat:function (array,...values) {  
+      for (let i of values){
+        array = array.concat(i)
+      }
+      return array
     }
 }
